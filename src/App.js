bc9b04da1579839c5c79e36d9af36be38165bb15
Jocelyn Tan <jocelyntan89@hotmail.com>
2021-01-27 22:10:06 +0100

    fetch data from pokeAPI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import logo from './pokemon-logo.svg'
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=40')
+      .then(response => response.json())
+      .then(listOfPokemon => {
+        const pokemonContainer = []
+
+        listOfPokemon.results.forEach(pokemon => {
+          fetch(pokemon.url)
+            .then(response => response.json())
+            .then(singlePokemon => pokemonContainer.push(singlePokemon))
+        })
+        
+        setData(pokemonContainer)
+      })
+  }, [])
+
   return (
     <div className="App">
       <Header>
